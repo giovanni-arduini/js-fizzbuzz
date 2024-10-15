83cb38e6903fa0b77d90134333aa0910addb4856
giovanni-arduini <gionnibigudd@gmail.com>
2024-10-15 14:37:03 +0200

    ADD const resto3 - resto3

--- app.js
+++ app.js
@@ -10,12 +10,17 @@ multipli di 3 che di 5 stampi FizzBuzz. */
 
 // Devo stampare numeri da 1 a 100
 
+let num = i;
+
 for (i = 0; i < 100; i++) {
   console.log(i + 1);
 }
 
 // Per sapere se numero è multiplo di n
 // Dato un numero devo sapere quale sia il uso resto se diviso per n
+
+const resto3 = num % 3; // [0,1,2]
+const resto5 = num % 5; // [0,1,2,3,4]
 
 // SE è multiplo di 3
 //      => Dovrò stampare "Fizz"
