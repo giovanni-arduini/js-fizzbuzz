7e69382705d02568a84a7562b337bf956a26565c
giovanni-arduini <gionnibigudd@gmail.com>
2024-10-15 14:34:28 +0200

    ADD for cycle 1-100

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ ma per i multipli di 3 stampi “Fizz” al posto del numero e
 per i multipli di 5 stampi Buzz. Per i numeri che sono sia
 multipli di 3 che di 5 stampi FizzBuzz. */
 
+//
+//
+
+// Devo stampare numeri da 1 a 100
+
+for (i = 0; i < 100; i++) {
+  console.log(i + 1);
+}
+
 // Per sapere se numero è multiplo di n
 // Dato un numero devo sapere quale sia il uso resto se diviso per n
 
